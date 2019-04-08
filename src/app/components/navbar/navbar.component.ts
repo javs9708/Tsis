c34db7a51d241034f38be88e1f03aa9a69c508db
Alexande Villegas <villegassanchezjhonalexander@gmail.com>
2019-04-07 22:58:18 -0500

    DoCheck

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit, DoCheck} from '@angular/core';
 import {AuthService} from '../../services/auth.service';
 import { Observable, of } from 'rxjs';
 import { AngularFirestore } from 'angularfire2/firestore';
@@ -9,31 +9,33 @@ import { AngularFirestore } from 'angularfire2/firestore';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit{
+export class NavbarComponent implements DoCheck{
 
   public data:any;
   public showNameA=[];
   public showName: Observable<any[]>;
 
   constructor(private  authService:  AuthService, private db: AngularFirestore) {
-    this.data = JSON.parse((localStorage.getItem('user')));
-    this.showName = this.db.collection('/usuarios').valueChanges();
-    this.showName.subscribe(data => {
-      if (data) {
-        this.showNameA = data;
-      }
-      for(let e of this.showNameA){
-        if(e.uid==this.data.uid){
-          this.data.displayName=e.name;
-          localStorage.setItem('user', JSON.stringify(this.data));
-          break;
-        }
-      }
 
-    });
+    // this.showName = this.db.collection('/usuarios').valueChanges();
+    // this.showName.subscribe(data => {
+    //   if (data) {
+    //     this.showNameA = data;
+    //
+    //   }
+    //   for(let e of this.showNameA){
+    //     if(e.uid==this.data.uid){
+    //       this.data.displayName=e.name;
+    //       localStorage.setItem('user', JSON.stringify(this.data));
+    //       break;
+    //     }
+    //   }
+    //
+    // });
   }
 
-  ngOnInit() {
+  ngDoCheck() {
+    this.data = JSON.parse((localStorage.getItem('user')));
     }
 
 
